45c8ac4866353211a7149c8fe307ca22c7f48b5e
Tanishk Parihar <30891813+tan305@users.noreply.github.com>
2017-12-31 10:26:25 +0530

    Add files via upload

---
+++ build.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def load_data():
+    """
+        Enter your code here
+    """
+
+
+def first_country(df):
+    """
+        Enter your code here
+    """
+
+
+def gold_medal(df):
+    """
+        Enter your code here
+    """
+
+
+def biggest_difference_in_gold_medal(df):
+    """
+        Enter your code here
+    """
+
+def get_points(df):
+    """
+    Enter your code here
+    """
+
+
+# df = load_data()
+# print(first_country(df)["# Summer"])
+# print(gold_medal(df))
+# print(biggest_difference_in_gold_medal(df))
+# print(get_points(df))
